bb7943635beb6e496ed8fefe78830759a571aded
Keegan <Keeg9087@gmail.com>
2020-07-19 14:08:35 -0500

    access current user on homepage

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import app from './base'
+import Header from './Header'
+import { AuthContext } from "./utils/auth";
+import * as firebase from 'firebase'
+
 
 const Home = () => {
+
+    const { currentUser } = useContext(AuthContext);
+
+    useEffect(() => {
+        firebase.database().ref('users/' + currentUser.uid).set({
+            email: currentUser.email
+        })
+    }, []);
+
+
+
+
+
+
+
     return (
         <>
-            <h1>Home</h1>
-            <button onClick={() => app.auth().signOut()}>Sign Out</button>
+            <Header>
+                <h1>Welcome, {currentUser.email}</h1>
+                <button onClick={() => app.auth().signOut()}>Sign Out</button>
+            </Header>
         </>
     )
 }
