a1268a0716b402281d053fc9bfd26534b250564f
Keegan <Keeg9087@gmail.com>
2020-07-25 17:04:21 -0500

    roompage init

---
+++ src/RoomPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Component } from 'react'
+
+
+class RoomPage extends Component {
+
+
+    constructor() {
+        super()
+        this.state = {
+            amount: 0
+        }
+    }
+
+    componentDidMount() {
+
+        this.setState({
+            amount: this.props.location.state.amount
+        })
+    }
+    render() {
+        return (
+            <>
+                <h1>{this.state.amount}</h1>
+            </>
+        )
+    }
+
+}
+
+export default RoomPage
