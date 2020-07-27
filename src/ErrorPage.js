699e8f21fb5d89a5e8f0aaa0fba34a2634b25eb7
Keegan <Keeg9087@gmail.com>
2020-07-27 13:42:16 -0500

    404 page

---
+++ src/ErrorPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class ErrorPage extends Component {
+
+
+
+
+    render() {
+        return (
+            <>
+                <h1 style={{ fontSize: '100px', textAlign: 'center' }}>404</h1>
+                <h2 style={{ textAlign: 'center' }}>Uh oh, looks like you took a wrong turn</h2>
+            </>
+        )
+    }
+}
+
+
+export default ErrorPage
